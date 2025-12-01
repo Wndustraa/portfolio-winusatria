fe761b2bbdd281571b7ab91bc6084d23b5567687
Windu Satria <windu.satria73@smk.belajar.id>
2025-12-01 07:47:56 +0700

    Update hero.tsx

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1vjtWfqYGMIMK_rlBnmDEpKco9MiEaQss/view"
+                    "https://drive.google.com/file/d/1T1fXH2V9yf5RkUkQv0hUS8qP_eyfdGqb/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
